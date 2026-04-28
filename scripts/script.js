d9b0a6933c1b525513d36a69d6695b0da1ba757e
Juan Diego <jd@Juan-Diegos-MacBook-Pro-M2-Pro.local>
2026-04-27 21:29:49 -0300

    implementar play and pause button com o js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const pauseButton = document.getElementsByClassName("pause-button")[0]
+const heroVideo = document.getElementsByTagName("video")[0]
+
+pauseButton.addEventListener("click", () => {
+  if (heroVideo.paused) {heroVideo.play(); pauseButton.src = "../assets/pause-button-128.png"; return} 
+  heroVideo.pause(); pauseButton.src = "../assets/play-button-128.png";
+})
