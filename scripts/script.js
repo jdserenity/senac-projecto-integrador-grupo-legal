756ae7d077533fa0026f954a4d88731676a32660
JvMaia1 <vmals@hotmail.com>
2026-04-28 21:40:35 -0300

    mudando o estilo do header da pagina principal;

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
+'use strict'
+
 const pauseButton = document.getElementsByClassName("pause-button")[0]
 const heroVideo = document.getElementsByTagName("video")[0]
+
+heroVideo.playbackRate = 0.75;
 
 pauseButton.addEventListener("click", () => {
   if (heroVideo.paused) {heroVideo.play(); pauseButton.src = "../assets/pause-button-128.png"; return} 
