72adce4954388a05a93886688c227b4f0c40c6ef
ysouza336 <yurisilva474@gmail.com>
2026-05-04 11:32:32 -0300

    update-index

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,31 @@ pauseButton.addEventListener("click", () => {
   if (heroVideo.paused) {heroVideo.play(); pauseButton.src = "../assets/pause-button-128.png"; return} 
   heroVideo.pause(); pauseButton.src = "../assets/play-button-128.png";
 })
+
+// Função para menu mobile
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+const links = menu.querySelectorAll("a");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  document.body.classList.add("locked");
+});
+
+// Fecha ao clicar em qualquer opção
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    fecharMenu();
+  });
+});
+
+// (Opcional) fecha clicando fora
+overlay.addEventListener("click", fecharMenu);
+
+function fecharMenu() {
+  menu.classList.add("hidden");
+  overlay.classList.add("hidden");
+  document.body.classList.remove("locked");
+}
