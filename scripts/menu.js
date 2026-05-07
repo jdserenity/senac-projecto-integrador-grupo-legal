99319b89a3e8edede095eb80ad158ebed4f95adc
ysouza336 <yurisilva474@gmail.com>
2026-05-07 09:22:19 -0300

    indexacao-menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,64 @@
 'use strict'
 
-//Botão Menu
-const hamburger = document.getElementById('hamburger');
-    const mobileMenu = document.getElementById('mobileMenu');
+// CARREGA MENU
+async function loadMenu() {
+  const response = await fetch('./components/menu.html');
+  const data = await response.text();
+  document.getElementById('menu-container').innerHTML = data;
 
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      mobileMenu.classList.toggle('active');
+  // AGORA O HTML EXISTE
+  startMenu();
+}
 
-      // BLOQUEIA SCROLL DO SITE
-      if(mobileMenu.classList.contains('active')){
-        document.body.style.overflow = 'hidden';
-      } else {
-        document.body.style.overflow = 'auto';
-      }
+loadMenu();
+
+//CARREGA FOOTER
+
+async function loadComponent(elementId, file) {
+  const response = await fetch(file);
+  const html = await response.text();
+
+  document.getElementById(elementId).innerHTML = html;
+}
+
+loadComponent('footer-container', './components/footer.html');
+
+
+// FUNÇÃO DO MENU
+function startMenu() {
+
+  const hamburger = document.getElementById('hamburger');
+
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  hamburger.addEventListener('click', () => {
+
+    hamburger.classList.toggle('active');
+
+    mobileMenu.classList.toggle('active');
+
+    // BLOQUEIA SCROLL
+    if (mobileMenu.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+  });
+
+  // FECHA MENU AO CLICAR
+  document.querySelectorAll('.mobile-menu a').forEach(link => {
+
+    link.addEventListener('click', () => {
+
+      mobileMenu.classList.remove('active');
+
+      hamburger.classList.remove('active');
+
+      document.body.style.overflow = 'auto';
+
     });
 
-    // FECHA MENU AO CLICAR EM UMA OPÇÃO
-    document.querySelectorAll('.mobile-menu a').forEach(link => {
-      link.addEventListener('click', () => {
-        mobileMenu.classList.remove('active');
-        hamburger.classList.remove('active');
-        document.body.style.overflow = 'auto';
-      });
-    });
+  });
+
+}
