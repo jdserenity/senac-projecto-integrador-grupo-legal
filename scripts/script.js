b408bb1730ff18307b9ff948af9a454a3c0923f0
JOAO VITOR HELIO COSTA MAIA <joao.vhcmaia@senacsp.edu.br>
2026-05-19 11:26:02 -0300

    finalizada estilização da pagina index

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,27 @@
 'use strict'
 
 const heroVideo = document.getElementsByTagName("video")[0];
-
+const titulo = document.querySelector('#titulo');
+const tituloNav = document.querySelector('#titulo-nav');
+const ancoraComoFunciona = document.querySelector('#ancora-como-funciona');
+const botaoRolagem = document.querySelector('#botao-rolar');
 heroVideo.playbackRate = 0.75;
+console.log(botaoRolagem);
 
+const tituloResponsivo = new IntersectionObserver (([entry]) => {
+    if (entry.isIntersecting){
+        tituloNav.classList.remove('visivel');
+    } else tituloNav.classList.add('visivel');
+    {
+        threshold: 0;
+    }
+});
+
+// const rolagemDinamica = new IntersectionObserver(([entry]) => {
+//     if (entry.isIntersecting){    
+//         botaoRolagem.href = 
+//     }
+// }
+// );
+
+tituloResponsivo.observe(titulo)
